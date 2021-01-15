279c5926fd27355c21241711123138cb07240645
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-01-16 00:42:53 +0100

    build(config): update grunt config

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,5 +1,11 @@
+const { env } = require('process');
+
+// eslint-disable-next-line padding-line-between-statements
+const filter = (predicate, ...tasks) => (predicate ? tasks : []);
+const isTarget = (...targets) => env.TARGET === undefined || targets.includes(env.TARGET);
+
 module.exports = {
     build: ['clean:build', 'sh:build-es2019', 'sh:build-es5', 'babel:build'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
-    test: ['build', 'sh:test-unit-browser', 'sh:test-unit-node']
+    test: ['build', ...filter(isTarget('chrome', 'firefox'), 'sh:test-unit-browser'), ...filter(isTarget('node'), 'sh:test-unit-node')]
 };
