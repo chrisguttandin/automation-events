a237a1a87c096e03b5eeed1d4a32e82101a35723
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-04-14 23:28:39 +0200

    refactor: delay upperIndex computation to only do it when necessary

--- src/functions/interpolate-value.ts
+++ src/functions/interpolate-value.ts
@@ -1,10 +1,11 @@
 export const interpolateValue = (values: number[] | Float32Array, theoreticIndex: number) => {
     const lowerIndex = Math.floor(theoreticIndex);
-    const upperIndex = Math.ceil(theoreticIndex);
 
-    if (lowerIndex === upperIndex) {
+    if (lowerIndex === theoreticIndex) {
         return values[lowerIndex];
     }
+
+    const upperIndex = Math.ceil(theoreticIndex);
 
     return (1 - (theoreticIndex - lowerIndex)) * values[lowerIndex] + (1 - (upperIndex - theoreticIndex)) * values[upperIndex];
 };
